31d9c93b5ac9803c3a34c810f0ec9512e63c5a00
briankag8 <b.gatanazi@alustudent.com>
2024-11-04 19:51:10 +0200

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0  # To keep track of the number of elements printed
-    for i in range(x):  # Iterate up to x times
-        try:
-            print(my_list[i], end='')  # Print the element followed by a space
-            count += 1  # Increment the count for each successful print
-        except IndexError:
-            break  # Exit the loop if an IndexError occurs (list index out of range)
-    
-    print()  # Print a newline after all elements have been printed
-    return count  # Return the number of elements printed
-
-result = safe_print_list([1, 2, 3, 4, 5], 10)
-print("Number of elements printed:", result)
-  
+    if my_list is None:
+        return 0
+    count = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        print()
+    except IndexError:
+        print()
+        return count
+    return count
