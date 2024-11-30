081c4848bf6f92b0df5d62ecbb1a858c658d58e2
briankag8 <b.gatanazi@alustudent.com>
2024-11-30 15:40:29 +0200

    8-json_api.py

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -4,24 +4,33 @@ takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter
 """
 import requests
-from sys import argv
+import sys
 
+def search_user(letter=""):
+    url = "http://0.0.0.0:5000/search_user"
+    
+    # Send the POST request with the letter as the query parameter
+    response = requests.post(url, data={'q': letter})
+    
+    try:
+        # Attempt to parse the JSON response
+        response_json = response.json()
+        
+        if response_json:
+            # If the response is not empty, display the id and name
+            print(f"[{response_json.get('id')}] {response_json.get('name')}")
+        else:
+            # If the response is empty, display 'No result'
+            print("No result")
+    
+    except ValueError:
+        # If JSON is invalid, display 'Not a valid JSON'
+        print("Not a valid JSON")
 
 if __name__ == "__main__":
-    """
-    takes in a letter and sends a POST request to
-    http://0.0.0.0:5000/search_user with the letter as a parameter
-    """
-    url = 'http://0.0.0.0:5000/search_user'
-    r = requests.get(url)
-    if len(argv) == 2:
-        r = requests.post(url, data={'q': argv[1]})
-    else:
-        r = requests.post(url, data={'q': ""})
-    try:
-        if r.json() == {}:
-            print("No result")
-        else:
-            print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
-    except:
-        print("Not a valid JSON")`
+    # If there's a command-line argument, use it as the letter
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    
+    # Call the search_user function with the letter
+    search_user(letter)
+
